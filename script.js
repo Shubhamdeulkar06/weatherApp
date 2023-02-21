040f3445378239789b43d6b648c4d039af0ab90c
Shubhamdeulkar06 <shuboy0868@gmail.com>
2023-02-21 11:31:03 +0530

    added try catch for error handling

--- script.js
+++ script.js
@@ -32,83 +32,88 @@ const nextSunset = document.querySelectorAll(".sunset");
 const tempLoading = document.querySelectorAll(".loading");
 
 const getData = async (event) => {
-  // document.body.innerHTML = "Loading....";
-  event.preventDefault();
-  if (!inputBox.value) {
-    alert("Please Enter The City Name: ");
-    return;
-  }
-
-  //
-  const city = inputBox.value;
-
-  // Fetch Details
-  countryName.innerHTML = "Updating....";
-  tempLoading.forEach((element) => {
-    element.innerText = "Updating....";
-  });
-  const fetchData = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=3b6fa2ae2fef43e3bb5163813231102&q=${city}&days=6`
-  );
-
-  const orgData = await fetchData.json();
-  data = orgData;
-  console.log(data);
-  if (weatherBox.submit) {
-    document.querySelector(".temp-container").style.display = "block";
-  } else {
-    document.querySelector(".temp-container").style.display = "none";
-  }
-  // Displaying the data in HTML
-
-  countryName.innerHTML = data.location.country;
-  stateName.innerHTML = data.location.region;
-  humidity.innerHTML = data.current.humidity;
-  windSpeed.innerHTML = `${data.current.wind_kph} Km/h`;
-  windDirection.innerHTML = data.current.wind_dir;
-  temprature.innerHTML = data.current.temp_c;
-  logoImage.src = data.current.condition.icon;
-  weatherStatus.innerHTML = data.current.condition.text;
-  timeZone.innerHTML = data.location.tz_id;
-  localTime.innerHTML = data.location.localtime;
-  sunrise.innerHTML = data.forecast.forecastday[0].astro.sunrise;
-  sunset.innerHTML = data.forecast.forecastday[0].astro.sunset;
-
-  //fetching location for each element
-  cityName.forEach((element) => {
-    element.innerHTML = data.location.name;
-  });
-
-  const getTemp = (num) => {
-    return data.forecast.forecastday[0].hour[num].temp_c;
-  };
-
-  // fetching temprature based on certain hour
-  const getHourlyTemp = () => {
-    hourlyTemp[0].innerHTML = `${getTemp(0)}<sup>o</sup>`;
-    hourlyTemp[1].innerHTML = `${getTemp(4)}<sup>o</sup>`;
-    hourlyTemp[2].innerHTML = `${getTemp(8)}<sup>o</sup>`;
-    hourlyTemp[3].innerHTML = `${getTemp(12)}<sup>o</sup>`;
-    hourlyTemp[4].innerHTML = `${getTemp(16)}<sup>o</sup>`;
-    hourlyTemp[5].innerHTML = `${getTemp(20)}<sup>o</sup>`;
-    hourlyTemp[6].innerHTML = `${getTemp(23)}<sup>o</sup>`;
-  };
-  getHourlyTemp();
-  const futureForcast = () => {
-    for (let i = 0; i < 5; i++) {
-      futureDate[i].innerText = data.forecast.forecastday[i + 1].date;
-      minTemp[i].innerHTML = `${
-        data.forecast.forecastday[i + 1].day.mintemp_c
-      } <sup>o</sup>C`;
-      maxTemp[i].innerHTML = `${
-        data.forecast.forecastday[i + 1].day.maxtemp_c
-      } <sup>o</sup>C`;
-      nextSunrise[i].innerHTML = data.forecast.forecastday[i + 1].astro.sunrise;
-      nextSunset[i].innerHTML = data.forecast.forecastday[i + 1].astro.sunset;
+  try {
+    event.preventDefault();
+    if (!inputBox.value) {
+      alert("Please Enter The City Name: ");
+      return;
     }
-  };
 
-  futureForcast();
+    //
+    const city = inputBox.value;
+
+    // Fetch Details
+    countryName.innerHTML = "Updating....";
+    tempLoading.forEach((element) => {
+      element.innerText = "Updating....";
+    });
+    const fetchData = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=3b6fa2ae2fef43e3bb5163813231102&q=${city}&days=6`
+    );
+
+    const orgData = await fetchData.json();
+    data = orgData;
+    console.log(data);
+    if (weatherBox.submit) {
+      document.querySelector(".temp-container").style.display = "block";
+    } else {
+      document.querySelector(".temp-container").style.display = "none";
+    }
+    // Displaying the data in HTML
+
+    countryName.innerHTML = data.location.country;
+    stateName.innerHTML = data.location.region;
+    humidity.innerHTML = data.current.humidity;
+    windSpeed.innerHTML = `${data.current.wind_kph} Km/h`;
+    windDirection.innerHTML = data.current.wind_dir;
+    temprature.innerHTML = data.current.temp_c;
+    logoImage.src = data.current.condition.icon;
+    weatherStatus.innerHTML = data.current.condition.text;
+    timeZone.innerHTML = data.location.tz_id;
+    localTime.innerHTML = data.location.localtime;
+    sunrise.innerHTML = data.forecast.forecastday[0].astro.sunrise;
+    sunset.innerHTML = data.forecast.forecastday[0].astro.sunset;
+
+    //fetching location for each element
+    cityName.forEach((element) => {
+      element.innerHTML = data.location.name;
+    });
+
+    const getTemp = (num) => {
+      return data.forecast.forecastday[0].hour[num].temp_c;
+    };
+
+    // fetching temprature based on certain hour
+    const getHourlyTemp = () => {
+      hourlyTemp[0].innerHTML = `${getTemp(0)}<sup>o</sup>`;
+      hourlyTemp[1].innerHTML = `${getTemp(4)}<sup>o</sup>`;
+      hourlyTemp[2].innerHTML = `${getTemp(8)}<sup>o</sup>`;
+      hourlyTemp[3].innerHTML = `${getTemp(12)}<sup>o</sup>`;
+      hourlyTemp[4].innerHTML = `${getTemp(16)}<sup>o</sup>`;
+      hourlyTemp[5].innerHTML = `${getTemp(20)}<sup>o</sup>`;
+      hourlyTemp[6].innerHTML = `${getTemp(23)}<sup>o</sup>`;
+    };
+    getHourlyTemp();
+    const futureForcast = () => {
+      for (let i = 0; i < 5; i++) {
+        futureDate[i].innerText = data.forecast.forecastday[i + 1].date;
+        minTemp[i].innerHTML = `${
+          data.forecast.forecastday[i + 1].day.mintemp_c
+        } <sup>o</sup>C`;
+        maxTemp[i].innerHTML = `${
+          data.forecast.forecastday[i + 1].day.maxtemp_c
+        } <sup>o</sup>C`;
+        nextSunrise[i].innerHTML =
+          data.forecast.forecastday[i + 1].astro.sunrise;
+        nextSunset[i].innerHTML = data.forecast.forecastday[i + 1].astro.sunset;
+      }
+    };
+
+    futureForcast();
+  } catch (error) {
+    alert("Please check the spelling of the city you have entered");
+    location.reload();
+  }
 };
 
 let date = new Date();
